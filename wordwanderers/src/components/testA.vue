b49688f951376daa3e080b5e18e54db26d3e1934
Kristian <1589686086@qq.com>
2023-04-25 17:17:49 +0800

    testA

--- wordwanderers/src/components/testA.vue
+++ wordwanderers/src/components/testA.vue
@@ -2,10 +2,15 @@
 <div>
 {{ cnt }}
 </div>
+<button @click="add">
+
+</button>
 </template>
 
 <script setup>
 import { ref } from "vue";
 const cnt = ref(0);
-
+const add = () => {
+    cnt.value+=1;
+}
 </script>

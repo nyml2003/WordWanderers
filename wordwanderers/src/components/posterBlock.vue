a0aca45b5fbd5c376185ed7876145ac8cabe6511
Kristian <1589686086@qq.com>
2023-04-25 19:56:36 +0800

    425zct

---
+++ wordwanderers/src/components/posterBlock.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="poster">
+      <!-- 使用 div 元素创建一个矩形盒子 -->
+        <div class="poster_words">
+            <!-- 在盒子中放置一些文本 -->
+            <p class="p_w_black">Write Words</p>
+            <p class="p_w_blue">Be A Wanderer</p>
+        </div>
+        <div class="poster_box">
+            <a class="white" href="login.html">Login Now →</a>
+        </div>
+    </div>
+    <div style="text-align: center;">
+            <img class="p_img" src="@/assets/poster.png">
+    </div>
+</template>
+
+<style >
+    @import url(@/assets/css/poster.css);
+</style>
--- wordwanderers/src/views/HomeView.vue
+++ wordwanderers/src/views/HomeView.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="home">
-  </div>
+    <posterBlock></posterBlock>
+  
 </template>
 
 <script>
 // @ is an alias to /src
 
+import posterBlock from "@/components/posterBlock.vue"
+
 export default {
   name: 'HomeView',
   components: {
+    posterBlock
   }
 }
 </script>

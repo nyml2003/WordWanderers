e8675177adf67e6e5c2ac2fecabfe70bbc9a1ffd
ttishappy <1589686086@qq.com>
2023-04-29 22:01:01 +0800

    zcttt

--- wordwanderers/src/views/BlogPresent.vue
+++ wordwanderers/src/views/BlogPresent.vue
@@ -1,46 +1,61 @@
 <template>
-    <div>
-        <el-card class="box-card"  @click="$router.push({ name: 'BlogPresent', params: { blogId: blog.id } })">
-            <template #header>
-            <div class="card-header">
-                <h3>{{ blog.title }}{{ blog.id }}</h3>
-                <p class="author">作者：{{ blog.user }}{{ blog.id }}</p>
-            </div>
-            </template>
-            <p style="font-size: xx-small;color:grey" >{{ blog.content }}</p>
-        </el-card>
+  <div>
+    <div class="box-card">
+      <el-card @click="$router.push({ name: 'BlogPresent', params: { blogId: blog.id } })">
+        <template #header>
+          <div class="card-header">
+            <h3>{{ blog.title }}{{ blog.id }}</h3>
+            <p class="author">作者：{{ blog.user }}{{ blog.id }}</p>
+          </div>
+        </template>
+        <p style="font-size: xx-small;color:grey">{{ blog.content }}</p>
+      </el-card>
+      <div class="info">
+        <el-button type="primary">
+          <el-icon-thumb-up></el-icon-thumb-up> 
+          {{ blog.likes }}
+        </el-button>
+        <el-button type="info">
+          <el-icon-view></el-icon-view> 
+          {{ blog.views }}
+        </el-button>
+      </div>
     </div>
+  </div>
 </template>
-  
-<script>
-  export default {
-    name: 'BlogPresent',
-    data() {
-      return {
-        blog: {},
-      };
-    },
-    mounted() {
-      // 根据 this.$route.params.blogId 获取博文数据的方法，例如发送 API 请求获取数据
-      // 将获取的数据赋值给 this.blog
-      const blogId = this.$route.params.blogId;
-      // 需要改这里！！！！！！！！！！！
-      // 发送 API 请求或其他操作来获取博文数据
 
-      // 这里只是示例，你需要根据实际情况获取博文数据并赋值给 this.blog
-          this.blog = { id: blogId, title: '测试博文', user: '作者', content: '博文内容' };
-    },
-  };
-  </script>
-  
-  <style scoped>
-    .box-card{
-        margin-top: 20px;
-        margin-left: 5%;
-        margin-right: 5%;
-    }
-    .author{
-        font-size: x-small;
-        color: grey;
-    }
+<script>
+import { ElIconThumbUp, ElIconView } from '@element-plus/icons';
+
+export default {
+  name: 'BlogPresent',
+  components: {
+    ElIconThumbUp,
+    ElIconView
+  },
+  data() {
+    return {
+      blog: {},
+    };
+  },
+  mounted() {
+    const blogId = this.$route.params.blogId;
+    this.blog = { id: blogId, title: '测试博文', user: '作者', content: '博文内容', likes: 10, views: 100 };
+  },
+};
+</script>
+
+<style scoped>
+.box-card {
+  margin-top: 20px;
+  margin-left: 5%;
+  margin-right: 5%;
+}
+.author {
+  font-size: x-small;
+  color: grey;
+}
+.info {
+  margin-top: 20px;
+}
 </style>

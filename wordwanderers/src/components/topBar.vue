8159d1d919e8350f7cf0412bf5aa4420b2608746
ttishappy <1589686086@qq.com>
2023-04-29 16:17:40 +0800

    blog-first zct

--- wordwanderers/src/components/topBar.vue
+++ wordwanderers/src/components/topBar.vue
@@ -5,6 +5,7 @@
       <div class="nav-links">
         <router-link to="/">Home</router-link>
         <router-link to="/about">About</router-link>
+        <router-link to="/blog">Blog</router-link>
       </div>
       <div class="search-box">
         <i class="fa fa-search"></i>
